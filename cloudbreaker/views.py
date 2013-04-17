71421631106a965b0f42cc87fe555aacd7e7c954
Ryan Winkelmaier <rwinkelm@cs.nmt.edu>
2013-04-17 16:05:56 -0600

    Started creating /

--- cloudbreaker/views.py
+++ cloudbreaker/views.py
@@ -1,6 +1,10 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='projectView', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'cloudbreaker'}
+
+@view_config(route_name='root', renderer='templates/root_template.pt')
+def root_view(request):
+    return {}
