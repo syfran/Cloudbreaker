f1e93eb8a838104745671d128053dc91fe17f4b4
Ryan <rwinkelm@cs.nmt.edu>
2013-04-17 16:36:23 -0600

    removed the starting page

--- cloudbreaker/__init__.py
+++ cloudbreaker/__init__.py
@@ -6,7 +6,6 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('projectView', '/project')
     config.add_route('root', '/')
     config.scan()
     return config.make_wsgi_app()
--- cloudbreaker/views.py
+++ cloudbreaker/views.py
@@ -1,10 +1,5 @@
 from pyramid.view import view_config
 
-
-@view_config(route_name='projectView', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'cloudbreaker'}
-
 @view_config(route_name='root', renderer='templates/root_template.pt')
 def root_view(request):
     return {}
