88d22187dac67bc37ea111ef721149799fdd183c
Ryan <rwinkelm@cs.nmt.edu>
2013-04-20 03:02:31 -0600

    more changes to hashmanager

--- cloudbreaker/hashmanager.py
+++ cloudbreaker/hashmanager.py
@@ -4,32 +4,46 @@ Keep track of hashes and workshares
 
 hashes = []
 
-class workshare:
+class Workshare:
     """
     Class to encapsulate workshares
     """
-    def __init__(self, hashstring, hashmodule, passwordsource, start, size):
+    def __init__(self, hashstring, hashtype, passwordsource, start, size):
         self.hashstring = hashstring
-        self.hashmodule = hashmodule
+        self.hashtype = hashtype
         self.passwordsource = passwordsource
         self.start = start
         self.size = size
 
-class hashtracker:
+class HashTracker:
     """
     Represent a submitted hash
     """
 
-    def __init__(self, hashstring, hashmodule, passwordsource):
+    def __init__(self, hashstring, hashtype, source):
         self.hashstring = hashstring
-        self.hashmodule = hashmodule
-        self.passwordsource = passwordsource
+        self.hashtype = hashtype
+        self.source = source
+        self.state = 0
+
+        # hard coded for now
+        self.sharesize = 30000
+        self.lock = Lock()
 
     def get_workshare(self):
         """
-        Get a new workshare for this hash
-        """
-        pass
+        Get a new workshare for this hash """ # Keep this synchronized
+        with lock:
+            if self.state + self.hashsize > self.source.size:
+                this_sharesize = self.source.size - self.state
+            else:
+                this_sharesize = self.sharesize
+            workshare = Workshare(self.hashstring, 
+                self.hashtype, self.source, self.state, this_sharesize)
+            self.state += this_sharesize
+
+    def get_progress(self):
+        return 
 
     def to_dict(self):
         return {
