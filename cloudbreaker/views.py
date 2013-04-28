4b47eacad6c50c3b33e94f162567f307ba507b28
Ryan Winkelmaier <rwinkelm@cs.nmt.edu>
2013-04-28 08:21:42 -0600

    Added auth to all pages

--- cloudbreaker/views.py
+++ cloudbreaker/views.py
@@ -7,12 +7,22 @@ from .hashmanager import *
 from .machines import machines
 from .amazon import *
 
+@view_config(context=HTTPForbidden)
+def auth_test(request):
+    response = HTTPUnauthorized()
+    response.headers.update(forget(request))
+    return response
+
 @view_config(route_name='root', renderer='templates/root_template.pt')
 def root_view(request):
+    if authenticated_userid(request) is None:
+        raise HTTPForbidden()
     return {"sources":sources.keys(), "hashtypes":hashtypes}
 
 @view_config(route_name='getinfo', renderer='json')
 def get_hashes_view(request):
+    if authenticated_userid(request) is None:
+        raise HTTPForbidden()
     return {"hashes":list(map(lambda x: x.to_dict(), hashes.values())),
         "machines":list(map(lambda x: x.to_dict(), machines.values())), 
         "spotprice":get_spot_price()}
@@ -20,9 +30,7 @@ def get_hashes_view(request):
 @view_config(route_name='cancelhash')
 def cancel_hash_view(request):
     if authenticated_userid(request) is None:
-        response = HTTPUnauthorized()
-        response.headers.update(forget(request))
-        return response
+        raise HTTPForbidden()
 
     if "hash" in request.params:
         remove_hash(request.params["hash"])
@@ -69,15 +77,14 @@ def complete_workshare_view(request):
         hashes[hash_string].complete_hash(request.params['password']) 
         
     machine.contact()
-    machine.complete_workshare(hash_string, workshare_start)
+    machine.complete_workshare(hash_string, workshare_start, num_hashes)
     return Response()
 
 @view_config(route_name='submithash')
 def submithash_view(request):
     if authenticated_userid(request) is None:
-        response = HTTPUnauthorized()
-        response.headers.update(forget(request))
-        return response
+        raise HTTPForbidden()
+
     try:
         hashstring = request.params['hash']
         hashtype = request.params['type'] 
