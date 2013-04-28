844d4ba9bfe09d3338ea0c5ae70a7888a6c194b0
Ryan Winkelmaier <rwinkelm@cs.nmt.edu>
2013-04-28 15:38:43 -0600

    Another syntax err

--- amiscripts/cpucloudbreaker.py
+++ amiscripts/cpucloudbreaker.py
@@ -25,8 +25,7 @@ while True:
     cmd_args["format"] = "sha512crypt"
     cmd_args["dict"] = dict_filename
 
-    with tempfile.NamedTemporaryFile() as passf, tempfile.NamedTemporaryFile() as potf, 
-      tempfile.NamedTemporaryFile() as wordlist:
+    with tempfile.NamedTemporaryFile() as passf, tempfile.NamedTemporaryFile() as potf, tempfile.NamedTemporaryFile() as wordlist:
 
         cmd_args["passfile"] = passf.name
         cmd_args["potfile"] = potf.name
