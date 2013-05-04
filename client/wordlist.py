9789766dc65bcda30fa8aee4ebb0bbbefa01875d
Ryan Winkelmaier <rwinkelm@cs.nmt.edu>
2013-05-04 17:04:59 -0600

    Created new seperate wordlist source

---
+++ client/wordlist.py
@@ -0,0 +1,50 @@
+"""
+Provide a wordlist as a source for passwords
+"""
+
+import tempfile
+import subprocess as sp
+
+from .config import john_bin, john_conf
+
+# Provide a mapping from the wordlist's name to it's location
+wordlist_lookup = {"cain":"/home/ubuntu/cain.txt"}
+
+# Commands
+wordlist_slice_cmd = "tail -n +%(start)s %(dict_file)s | head -n %(size)s"
+john_mangle_cmd = john_bin + " -pipe -stdout -rules --session=%(session)s-rule"
+
+class Wordlist:
+    """
+    Represents a wordlist password source. The rules argument may be used to
+    specify we don't want to apply rule mangling
+    """
+    def __init__(self, wordlist_name, start, size, session, rules=True):
+        self.cmd_args = {"dict_file": wordlist_lookup[wordlist_name],
+                         "start":start,
+                         "size":size,
+                         "session":session}
+        self.rules = rules
+        self.devnull = open("/dev/null")
+
+    def get_pipe(self, wordlist=None):
+        """
+        Return a pipe output of the wordlist. If the wordlist file is given, it
+        will also output the list to that file
+        """
+        slice_proc = sp.Popen(wordlist_slice_cmd % self.cmd_args, shell=True,
+            stdout=sp.PIPE, stderr=self.devnull)
+        if self.rules:
+            mangle_proc = sp.Popen(john_mangle_cmd % self.cmd_args, shell=True,
+                stdin = slice_proce.stdout, stdout=sp.PIPE,
+                stderr=self.devnull)
+            out = mangle_proce.stdout
+        else:
+            out = slice_proc.stdout
+
+        # Use tee to split the wordlist off
+        if wordlist is not None:
+            save_proc = sp.Popen(["tee", wordlist.name], stdin=out, stdout=sp.PIPE, stderr=self.devnul)
+            out = save_proc.stdout
+
+        return out
