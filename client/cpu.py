c3784d912ce9087dbc51df05af7f5a7adc41e108
Ryan Winkelmaier <rwinkelm@cs.nmt.edu>
2013-05-04 18:58:21 -0600

    Removed useless mangle variable

--- client/cpu.py
+++ client/cpu.py
@@ -15,8 +15,6 @@ john_session = "cpu-session"
 
 dict_filename = "/home/ubuntu/cain.txt"
 
-john_mangle_cmd = john_bin + " -pipe -stdout -rules --session=%(session)s-rule | tee %(wordlist)s"
-
 john_command = john_bin + " -pipe --format=%(format)s --nolog --pot=%(potfile)s --session=%(session)s %(passfile)s"
 
 # This hack allows us to get a smaller workshare if we're on a t1.micro
